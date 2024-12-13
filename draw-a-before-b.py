a611ac73471ac05ab64769b75b45828248f58afa
yzemaze <dev@yzemaze.de>
2024-12-13 12:20:22 +0100

    include tiles_C (positive for both)

--- draw-a-before-b.py
+++ draw-a-before-b.py
@@ -4,41 +4,40 @@ import random
 
 
 def calculate(tiles_remaining, tiles, iterations):
-    if tiles_remaining < tiles['A'] + tiles['B']:
-        raise ValueError("tiles_remaining < tiles['A'] + tiles['B']")
+    if tiles_remaining < tiles['A'] + tiles['B'] + tiles['C']:
+        raise ValueError(
+            "tiles_remaining < tiles['A'] + tiles['B'] + tiles['C']")
 
-    tile_list = ['A'] * tiles['A'] + ['B'] * tiles['B'] + \
-        ['N'] * (tiles_remaining - tiles['A'] - tiles['B'])
+    tile_list = ['A'] * tiles['A'] + ['B'] * tiles['B'] + ['C'] * tiles['C'] + \
+        ['N'] * (tiles_remaining - tiles['A'] - tiles['B'] - tiles['C'])
     half = math.ceil(tiles_remaining / 2)
+    index = {"A": 0, "B": 0, "C": 0}
     successes = {"A": 0, "B": 0}
     waits = {"countA": 0, "sumA": 0, "countB": 0, "sumB": 0}
 
     for _ in range(iterations):
         random.shuffle(tile_list)
-        if tiles['B']:
-            tile_list_a = tile_list[:half]
-            tile_list_b = tile_list[half:tiles_remaining]
-            if "A" in tile_list_a:
-                if "B" in tile_list_b[:tile_list_a.index("A")]:
-                    waits['countB'] += 1
-                    waits['sumB'] += tile_list_b.index("B")
-                    successes['B'] += 1
-                else:
-                    waits['countA'] += 1
-                    waits['sumA'] += tile_list_a.index("A")
-                    successes['A'] += 1
-            elif "B" in tile_list_b:
+        tile_list_a = tile_list[:half]
+        tile_list_b = tile_list[half:tiles_remaining]
+        index['A'] = tile_list_a.index("A") if "A" in tile_list_a \
+            else tiles_remaining + 1
+        index['C'] = tile_list_a.index("C") if "C" in tile_list_a \
+            else tiles_remaining + 1
+        index['A'] = min(index['A'], index['C'])
+        index['B'] = tile_list_b.index("B") if "B" in tile_list_b \
+            else tiles_remaining + 1
+        index['C'] = tile_list_b.index("C") if "C" in tile_list_b \
+            else tiles_remaining + 1
+        index['B'] = min(index['B'], index['C'])
+        if min(index['A'], index['B']) < tiles_remaining:
+            if index['A'] <= index['B']:
+                waits['countA'] += 1
+                waits['sumA'] += index['A']
+                successes['A'] += 1
+            else:
                 waits['countB'] += 1
-                waits['sumB'] += tile_list_b.index("B")
+                waits['sumB'] += index['B']
                 successes['B'] += 1
-        elif tile_list.index("A") % 2 == 0:
-            waits['countA'] += 1
-            waits['sumA'] += tile_list.index("A") / 2
-            successes['A'] += 1
-        else:
-            waits['countB'] += 1
-            waits['sumB'] += tile_list.index("A") / 2
-            successes['B'] += 1
 
     probability = {
         "A": successes['A'] / iterations,
@@ -53,8 +52,8 @@ def calculate(tiles_remaining, tiles, iterations):
 def main(tiles_remaining, tiles, iterations):
     probability, avg_wait = calculate(tiles_remaining, tiles, iterations)
 
-    print(f"A draws 1 out of {tiles['A']} before B: {probability['A']:9.5f}")
-    print(f"B draws 1 out of {tiles['B'] if tiles['B'] else tiles['A']} before A: {probability['B']:9.5f}")
+    print(f"A draws 1 out of {tiles['A'] + tiles['C']} before B: {probability['A']:9.5f}")
+    print(f"B draws 1 out of {tiles['B'] + tiles['C']} before A: {probability['B']:9.5f}")
     if tiles['B']:
         print(f"neither draws successfully:  {1 - probability['A'] - probability['B']:9.5f}")
     print(f"moves to wait on average A:  {avg_wait['A']:9.5f}")
@@ -74,13 +73,18 @@ if __name__ == "__main__":
     parser.add_argument(
         "-a", "--tiles_a",
         type=int,
-        default=1,
+        default=0,
         help="Number of positive tiles for player A remaining.")
     parser.add_argument(
         "-b", "--tiles_b",
         type=int,
         default=0,
         help="Number of positive tiles for player B remaining. If TILES_B = 0 then players flip for TILES_A tiles.")
+    parser.add_argument(
+        "-c", "--tiles_c",
+        type=int,
+        default=1,
+        help="Number of positive tiles for both players. Set TILES_A and TILES_B to 0 to let both players flip for a specific number of tiles.")
     parser.add_argument(
         "-i", "--iterations",
         type=int,
@@ -90,5 +94,5 @@ if __name__ == "__main__":
 
     main(
         tiles_remaining=args.tiles_remaining,
-        tiles={"A": args.tiles_a, "B": args.tiles_b},
+        tiles={"A": args.tiles_a, "B": args.tiles_b, "C": args.tiles_c},
         iterations=args.iterations)
